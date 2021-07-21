05cf47c8f30df8655fd68c5784624503edd560e6
rajajipms <rajaji8610@gmail.com>
2021-07-21 19:09:42 +0530

    First_upload
    Java_ooad-resume1

---
+++ src/Library_Management/DB.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Library_Management;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+
+/**
+ *
+ * @author samji
+ */
+public class DB {
+    
+    
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+
+    public static int S_no;
+   public static String userName;
+   public static String passWord;
+   public static int balance=5000;
+   public static int minBal=500;
+   
+
+
+    
+}
